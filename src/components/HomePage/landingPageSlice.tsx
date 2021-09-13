d9678e9ed33f007ebd134ea56dfaae59426a2e06
YoussefWilliam <youssef.william997@gmail.com>
2021-09-13 17:30:00 +0200

    Create slicer for fetching repo details

---
+++ src/components/HomePage/landingPageSlice.tsx
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { DataInterface } from "../../common/interfaces";
+
+const initialState: DataInterface = {
+  status: "idle",
+  data: null,
+};
+
+export const fetchRepoDetails = createAsyncThunk(
+  "repos/fetchRepoDetails",
+  async ({username, reponame}: {username: string, reponame: string}) => {
+    let githubAPILink = process.env.REACT_APP_GITHUB_API;
+    const response = await fetch(`${githubAPILink}/repos/${username}/${reponame}`);
+    const data = await response.json();
+    return data;
+  }
+);
+
+export const repoDetialsSlice = createSlice({
+  name: "fetchRepoDetails",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRepoDetails.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchRepoDetails.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.data = action.payload;
+      })
+      .addCase(fetchRepoDetails.rejected, (state) => {
+        state.status = "failed";
+      });
+  },
+});
+
+export const repoDetailsInfo = (state: any) => state.repoDetails;
+
+const repoDetailsReducers = repoDetialsSlice.reducer;
+export default repoDetailsReducers;
