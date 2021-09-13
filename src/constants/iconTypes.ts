238cd62d4fdfc36a2eaef06c1b7b4e7d81bf8e3e
YoussefWilliam <youssef.william997@gmail.com>
2021-09-13 17:28:19 +0200

    Create icon library

---
+++ src/constants/iconTypes.ts
@@ -0,0 +1,22 @@
+export const iconLibrary = [
+  {
+    id: 1,
+    src: "https://cdn-icons-png.flaticon.com/512/924/924874.png",
+    alt: "icon 1",
+  },
+  {
+    id: 2,
+    src: "https://cdn-icons-png.flaticon.com/512/236/236832.png",
+    alt: "Icon 2",
+  },
+  {
+    id: 3,
+    src: "https://cdn-icons-png.flaticon.com/512/3011/3011270.png",
+    alt: "Icon 3",
+  },
+  {
+    id: 4,
+    src: "https://cdn-icons-png.flaticon.com/512/706/706807.png",
+    alt: "Icon 4",
+  },
+];
