777837638ec847fbfd7218ffa327e283e65bdbba
YoussefWilliam <youssef.william997@gmail.com>
2021-09-13 17:28:08 +0200

    Creating wording file

---
+++ src/constants/wording.ts
@@ -0,0 +1,20 @@
+export const landingPage = {
+  CUSTOMIZE: "Customize your repository",
+  USERNAME: "Username",
+  USERNAME_PLACEHOLDER: "Author name",
+  REPONAME: "Repo name",
+  CHOOSE_COLOR: "Choose a color",
+  CHOOSE_ICON: "Choose an icon",
+  GENERATE: "Generate link",
+  VALIDATION: "Please fill the missing labels",
+};
+
+export const repoPage = {
+  AUTHOR_NAME: "Author name",
+  REPO_DESCRIPTION: "Repo description",
+  PROGRAMMING: "Programming Language used",
+  REPO_DETILS: "Repo details",
+  NUMBER_STARS: "Number of stars",
+  STARS: "Stars",
+  CONTRIBUTERS: "Top 10 Contributers",
+};
