ea3f781931acf89d7475326f46c8a88c4e323d0d
YoussefWilliam <youssef.william997@gmail.com>
2021-09-13 17:28:34 +0200

    Creating color library

---
+++ src/constants/colorTypes.ts
@@ -0,0 +1,22 @@
+export const colorLibrary = [
+  {
+    id: "1",
+    name: "Blue",
+    className: "blue-500",
+  },
+  {
+    id: "2",
+    name: "Green",
+    className: "green-500",
+  },
+  {
+    id: "3",
+    name: "Yellow",
+    className: "yellow-500",
+  },
+  {
+    id: "4",
+    name: "Black",
+    className: "gray-700",
+  },
+];
